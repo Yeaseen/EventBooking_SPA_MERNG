282eead940b82c6bd314242f49d6cfa86a5abe42
Yeaseen <yeaseen.arafat96@gmail.com>
2021-05-14 23:34:07 +0600

    first graphql api testing at graphiql

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 require("dotenv").config()
-const app = express()
 
 const PORT = process.env.PORT || 5000
+const { graphqlHTTP } = require('express-graphql')
+const { buildSchema } = require('graphql')
 
+const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.use(bodyParser.json())
+app.use('/graphql', graphqlHTTP ({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () =>{
+            return ['No Country for Old Men', 'New York Gangs', 'There will be BLOOD']
+        },
+        createEvent: (args) =>{
+            const eventName = args.name 
+            return eventName
+        }
+    },
+    graphiql: true
+}))
 
 app.get('/', (req, res, next) =>{
-    res.send('WELCOME TO THE WORLD OF NODE SERVER')
+    res.send('Welcome to the NODE Server')
 })
+
 app.listen(PORT, () => {
 	console.log("Server is running on", PORT)
 })
