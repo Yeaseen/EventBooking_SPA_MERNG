b02a238728f4b2edb9aa906d244dbcf12617e650
Yeaseen <yeaseen.arafat96@gmail.com>
2021-05-16 00:46:54 +0600

    BookEvent + CancelEvent done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { query } = require('express')
 const express = require('express')
-require("dotenv").config()
+require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
 const { graphqlHTTP } = require('express-graphql')
@@ -11,35 +11,32 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 mongoose.connect(process.env.MONGOURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
 mongoose.connection.on('connected', () => {
-    console.log("Connected to mongodb !!")
+  console.log('Connected to mongodb !!')
 })
 mongoose.connection.on('error', (err) => {
-    console.log("err connecting", err)
+  console.log('err connecting', err)
 })
-
-
 
 const graphqlSchema = require('./graphql/schema/index')
 const graphqlResolvers = require('./graphql/resolvers/index')
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  '/graphql',
+  graphqlHTTP({
     schema: graphqlSchema,
     rootValue: graphqlResolvers,
     graphiql: true
-}))
-
+  })
+)
 
 app.get('/', (req, res, next) => {
-    res.send('Welcome to the NODE Server')
+  res.send('Welcome to the NODE Server')
 })
 
 app.listen(PORT, () => {
-    console.log("Server is running on", PORT)
+  console.log('Server is running on', PORT)
 })
-
-
-
--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -2,6 +2,14 @@ const { buildSchema } = require('graphql')
 
 module.exports = buildSchema(`
 
+type Booking {
+    _id: ID!
+    event: Event!
+    user: User!
+    createdAt: String!
+    updatedAT: String!
+}
+
 type Event {
     _id: ID!
     title: String!
@@ -29,11 +37,17 @@ input EventInput {
 }
 type RootQuery {
     events: [Event!]!
+    bookings: [Booking!]!
+    
 }
 type RootMutation {
     createEvent(eventInput: EventInput): Event
     createUser(userInput: UserInput): User
+    bookEvent(eventId: ID!): Booking!
+    cancelBooking(bookingId: ID!): Event!
+    
 }
+
 schema {
     query: RootQuery
     mutation: RootMutation
