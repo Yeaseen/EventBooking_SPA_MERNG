30c40f9a0adc7ffb4da893efe95b0fba0a39d864
Yeaseen <yeaseen.arafat96@gmail.com>
2021-05-24 01:43:31 +0600

    typescript_useRef_event

--- frontend-react/src/components/screens/Auth.tsx
+++ frontend-react/src/components/screens/Auth.tsx
@@ -1,21 +1,57 @@
 // eslint-disable-next-line
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useEffect, useState, useRef, FormEvent } from 'react'
 import { Link, withRouter } from 'react-router-dom'
 import './Auth.css'
 
 const AuthPage = () => {
+  const email = useRef<HTMLInputElement>(null)
+  const password = useRef<HTMLInputElement>(null)
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    if (email && email.current) {
+      email.current.focus()
+    }
+    if (password && password.current) {
+      password.current.focus()
+    }
+    console.log(email.current?.value)
+    console.log(password.current?.value)
+  }
+
   return (
     <div className="main-content">
-      <form className="auth-form">
+      <form className="auth-form" onSubmit={onSubmit}>
         <h2>SimpleEvent</h2>
-        <div className="form-control">
-          <label htmlFor="email">E-mail</label>
-          <input type="email" id="email" placeholder="name@gmail.com" />
+
+        <div className="form__group field">
+          <input
+            type="input"
+            ref={email}
+            className="form__field"
+            placeholder="Email"
+            name="email"
+            id="email"
+            required
+          />
+          <label htmlFor="email" className="form__label">
+            Email
+          </label>
         </div>
 
-        <div className="form-control">
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
+        <div className="form__group field">
+          <input
+            type="password"
+            ref={password}
+            className="form__field"
+            placeholder="Password"
+            name="password"
+            id="password"
+            required
+          />
+          <label htmlFor="password" className="form__label">
+            Password
+          </label>
         </div>
 
         <div className="form-actions">
